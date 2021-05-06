480250a064a4886e88c4e4ce4bfe7a735a0dbcc8
1800s <1800syunya@Gmail.comgit config --global user.name Gotoshunya>
2021-05-06 17:48:39 +0900

    5/6ファイル再度プッシュ

---
+++ JavaWcp/src/Sample.java
@@ -0,0 +1,6 @@
+public class Sample { //クラスブロック開始
+	public static void main(String[] args) { //メソッドブロック開始
+		System.out.println("Hello World"); //System.out.printlnで出力。
+		System.out.println("Hello Java");
+	}
+}
