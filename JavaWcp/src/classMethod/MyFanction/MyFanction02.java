46fa2352d1624a1c4c6a2c2b1802569984d80d75
1800s <1800syunya@Gmail.comgit config --global user.name Gotoshunya>
2021-05-06 19:49:12 +0900

    5/6 Java9章サンプルの2まで

---
+++ JavaWcp/src/classMethod/MyFanction/MyFanction02.java
@@ -0,0 +1,18 @@
+package classMethod.MyFanction;
+
+public class MyFanction02 {
+	public String name;
+	public int age;
+
+	public MyFanction02() {
+		name = "山田";
+		age = 20;
+	}
+
+	public MyFanction02(String name, int age) {
+		this.name = name;
+		this.age = age;
+		
+	
+	}
+}
---
+++ JavaWcp/src/classMethod/action/action01.java
@@ -0,0 +1,13 @@
+package classMethod.action;
+
+import classMethod.MyFanction.MyFanction02;
+
+	public class action01 {
+	    public static void main(String[] args) {
+	        MyFanction02 yamada = new MyFanction02();  //Human02クラスのインスタンスを生成
+	        System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+
+	        MyFanction02 sato = new MyFanction02("佐藤", 25); //Human02クラスのインスタンスを生成
+	        System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+	    }
+	}
