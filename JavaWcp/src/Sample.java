cfbeb7355230158123e04077a95eceb689850c8b
1800s <1800syunya@Gmail.comgit config --global user.name Gotoshunya>
2021-05-22 15:57:33 +0900

    5/22 Scanner ライブラリ　練習

--- JavaWcp/src/Sample.java
+++ JavaWcp/src/Sample.java
@@ -1,6 +1,16 @@
-public class Sample { //クラスブロック開始
-	public static void main(String[] args) { //メソッドブロック開始
-		System.out.println("Hello World"); //System.out.printlnで出力。
-		System.out.println("Hello Java");
-	}
-}
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String []args) {
+    	int max = Math.max(6, 57);
+    	System.out.println("最大値は" + max + "です");
+    	
+    	
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.print("あなたの名前は");
+    	String name = scanner.next();
+    	System.out.println("私の名前は" + name + "です");
+    	scanner.close();
+    }
+  }
