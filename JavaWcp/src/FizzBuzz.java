d137265b0f27d6d2a2f109dce79c71161607e539
1800s <1800syunya@Gmail.com>
2021-06-15 18:57:47 +0900

    6/15 Fix

--- JavaWcp/src/FizzBuzz.java
+++ JavaWcp/src/FizzBuzz.java
@@ -1,25 +1,26 @@
 import java.util.Random;
+
 public class FizzBuzz {
 	public static void main(String[] args) {
-	  int i = 0;
-	  int counter = 100;
-	  int numfizz = 3;
-	  int numbuzz = 5;
-	  String strfizz = "Fizz";
-	  String strbuzz = "Buzz";
-	  
-	  for (i = 1; i <= counter; i++) {
-		  Random rand = new Random();
-		  int randnum = rand.nextInt(100) + 1;//変数randnumも100回繰り返す。/
-		  if (randnum % (numfizz * numbuzz) == 0) {
-			  System.out.println(strfizz + strbuzz);
-		  }  else if (randnum % numfizz == 0) {
-			  System.out.println(strfizz);
-		  }  else if (randnum % numbuzz == 0) {
-			  System.out.println(strbuzz);
-		  }  else {
-			  System.out.println(randnum);
-		  }
-	  }
-  }
+		int i = 0;
+		int counter = 100;
+		int numfizz = 3;
+		int numbuzz = 5;
+		String strfizz = "Fizz";
+		String strbuzz = "Buzz";
+
+		for (i = 1; i <= counter; i++) {
+			Random rand = new Random();
+			int randnum = rand.nextInt(100) + 1;//変数randnumも100回繰り返す。/
+			if (randnum % (numfizz * numbuzz) == 0) {
+				System.out.println(strfizz + strbuzz);
+			} else if (randnum % numfizz == 0) {
+				System.out.println(strfizz);
+			} else if (randnum % numbuzz == 0) {
+				System.out.println(strbuzz);
+			} else {
+				System.out.println(randnum);
+			}
+		}
+	}
 }
