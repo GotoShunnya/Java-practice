8fc2232c9c2579c1d1c756373984bd33a1248ced
1800s <1800syunya@Gmail.comgit config --global user.name Gotoshunya>
2021-06-15 23:46:26 +0900

    6/15 ファイル名変更

---
+++ JavaWcp/src/classMethod/calculator/PerfectNumber.java
@@ -0,0 +1,37 @@
+package classMethod.calculator;//完全数判別プログラム/
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("完全数か判断したい整数を記入してください:");
+		int num = scanner.nextInt();
+		
+		if (isPerfectNumber(num)) {
+			System.out.println(num + "は完全数です。");
+		} else {
+			System.out.println(num + "は完全数ではありません");
+		}
+		scanner.close();
+	}
+	
+	static boolean isPerfectNumber(int num) {
+		int sum = 0;
+		for(int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
+				System.out.print(i + ",");
+				sum += i;
+			}
+		}
+		
+		System.out.println("(約数)");
+		System.out.println(sum +  "(自身の数を除いた約数の和)");
+		
+		if (sum == num) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
