6aadf16d23ae120542e8394469de09bb22c6b353
Gotoshunya <1800syunya@Gmail.com>
2021-07-03 00:48:20 +0900

    7/3 ドットインストール学習開始（Java）

---
+++ JavaWcp/src/DotInstall.java
@@ -0,0 +1,27 @@
+
+public class DotInstall {
+	public static void main(String[] args) {
+		//文字列
+		String msg = "Hello World Again";
+		System.out.println(msg);
+		
+		//文字 シングルコーテーションで囲う
+		char a = 'a';
+		
+		//整数　byte < shot < int < long
+		int number = 8;
+		long BigNumber = 999999999999L;
+		
+		//浮動少数　flote < double
+		double x = 5.5;
+		float y = 4.5F;
+		
+		//論理値　
+		boolean flag = true;//false
+		
+		//文字列
+		//改行 /n 
+		//タブ /t 
+		
+	}
+}
