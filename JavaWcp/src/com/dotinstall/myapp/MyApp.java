662703d56f513769aa0d3489e1519f96875b04f6
Gotoshunya <1800syunya@Gmail.com>
2021-07-09 23:39:43 +0900

    7/9 Lesson28 抽象クラスうまく実装できない。

--- JavaWcp/src/com/dotinstall/myapp/MyApp.java
+++ JavaWcp/src/com/dotinstall/myapp/MyApp.java
@@ -1,8 +1,8 @@
 //--Lesson23 パッケージを使ってみよう～--
 
 package com.dotinstall.myapp;//パッケージを使う場合は、フォルダ構造・ファイル名を各ファイルに指定しなければならない。
-import com.dotinstall.myapp.model.User;//インポート分を使えば指定したクラスを読み込める。
 import com.dotinstall.myapp.model.AdminUser;
+import com.dotinstall.myapp.model.User;//インポート分を使えば指定したクラスを読み込める。
 //import com.dotinstall.myapp.model.*; myapp.model以下のクラスをすべて読み込みたい場合、＊（ワイルドカード）を使う事ですべてのクラスを指定できる。
 
 public class MyApp {
@@ -11,11 +11,12 @@ public class MyApp {
 	User.getInfo();//0
 	  
     User tom = new User("tom", 65);
-    tom.setScore(85);
+    tom.setScore(85);//setter呼び出し
     tom.setScore(-67);
-    System.out.println(tom.getScore());
+    System.out.println(tom.getScore());//getter呼び出し
     //System.out.println(tom.name); 今回nameフィールドはアクセス修飾子にprotectedを使っているため、MyAppから直接アクセスできない。
     tom.sayHi();
+    
     User.getInfo();//1
     
     AdminUser bob = new AdminUser("bob", 65);
@@ -23,6 +24,10 @@ public class MyApp {
     //System.out.println(bob.name); 今回nameフィールドはアクセス修飾子にprotectedを使っているため、MyAppから直接アクセスできない。
     bob.sayHi();
     bob.sayHello();
+    
+//    JapaneseUser Bob = new JapaneseUser();
+//    AmericanUser Tom = new AmericanUser();
+//    Tom.sayHi();
+//    Bob.sayHi();
   }
-
 }
--- JavaWcp/src/com/dotinstall/myapp/model/User.java
+++ JavaWcp/src/com/dotinstall/myapp/model/User.java
@@ -3,12 +3,24 @@ package com.dotinstall.myapp.model;//パッケージを使う場合は、フォ�
 public class User {
   protected String name;//アクセス修飾子をprotectedに指定。これはこのクラス内と子クラスのみアクセス権が与えられる。※privateは指定したクラスでのみアクセスできる。
   protected int score;
-  private static int count = 0;//クラス変数。static修飾子を使うと、クラスに紐づいた形で値を保持できる。
+  private static int count;//クラス変数。static修飾子を使うと、クラスに紐づいた形で値を保持できる。
+  private static final double VERSION = 1.1;//finalは定数の宣言。基本的に定数名はすべて大文字。値を変更できない。またクラスに付与すると継承できなくなり、メソッドにつけるとオーバーライドできなくなることに注意。
+                                              //またフィールドには、staticと合わせて使われることがある。
 
-  public User(String name, int score) {//コンストラクタ
+  static {//staticイニシャライザ
+	  User.count = 0;//クラス変数が配列などで複数のデータを初期化する場合に用いられる。
+	  System.out.println("Static initializer");
+  }
+  
+  {//インスタンスイニシャライザ。インスタンス化される前に処理が行われる。
+	  System.out.println("Instance initializer");
+  }
+  
+  public User(String name, int score) {//コンストラクタ。こっちは、インスタンス化後に処理が行われる。
     this.name = name;
     this.score = score;
     User.count++;
+	System.out.println("Constructer");
   }
   
   public static void getInfo() {//クラスメソッド
