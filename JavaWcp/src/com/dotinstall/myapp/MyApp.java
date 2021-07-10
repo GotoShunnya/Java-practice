be9ad4e69173b473781d8b1fd83b7ab18fa764ec
Gotoshunya <1800syunya@Gmail.com>
2021-07-10 17:51:22 +0900

    7/10 Lesson30まで

--- JavaWcp/src/com/dotinstall/myapp/MyApp.java
+++ JavaWcp/src/com/dotinstall/myapp/MyApp.java
@@ -3,11 +3,27 @@
 package com.dotinstall.myapp;//パッケージを使う場合は、フォルダ構造・ファイル名を各ファイルに指定しなければならない。
 import com.dotinstall.myapp.model.AdminUser;
 import com.dotinstall.myapp.model.User;//インポート分を使えば指定したクラスを読み込める。
-//import com.dotinstall.myapp.model.*; myapp.model以下のクラスをすべて読み込みたい場合、＊（ワイルドカード）を使う事ですべてのクラスを指定できる。
 
 public class MyApp {
 
   public static void main(String[] args) {
+	
+	Print P = new Print();
+	P.print();
+	P.getInfo();
+	
+	Result res;//列挙型変数定義。
+	res = Result.SUCCESS;//	列挙型に定義されている定数SUCCESSをresに代入
+	switch (res){
+	  case SUCCESS:
+		  System.out.println("OK!");
+		  break;
+		  
+	  case ERROR:
+		  System.out.println("NG!");
+		  break;
+	}
+	
 	User.getInfo();//0
 	  
     User tom = new User("tom", 65);
@@ -25,9 +41,47 @@ public class MyApp {
     bob.sayHi();
     bob.sayHello();
     
-//    JapaneseUser Bob = new JapaneseUser();
-//    AmericanUser Tom = new AmericanUser();
-//    Tom.sayHi();
-//    Bob.sayHi();
+    JapaneseUser taka = new JapaneseUser();
+    AmericanUser mone = new AmericanUser();
+    taka.sayHi();
+    mone.sayHi();
   }
+}
+
+interface printable{
+	double VERSION = 1.1;//定数
+	void print();//抽象メソッド
+	public default void getInfo() {//defaultメソッド。直接実装が書けるメソッド。
+		System.out.println("I/F ver. " + printable.VERSION);
+	}
+	//staticメソッド。defaultメソッドが複雑化した時のヘルパーとして使われていることが多い。
+}
+
+class Print implements printable{
+	public void print(){
+	  System.out.println("Now printing user profile...");
+	}
+}
+
+abstract class Abstract{//抽象クラス
+	public abstract void sayHi();//抽象メソッド。
+}
+
+class JapaneseUser extends Abstract{
+	@Override
+	public void sayHi() {
+		System.out.println("こんにちは！");
+	}
+}
+
+class AmericanUser extends Abstract{
+	@Override
+	public void sayHi() {
+		System.out.println("HI！");
+	}
+}
+
+enum Result{//列挙型。定数をまとめたもの。
+	SUCCESS,//0   res.ordinalでオーディナル番号を取得できる。
+	ERROR,//1
 }
