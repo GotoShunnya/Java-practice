add7c214e7e2ec93ec924fe5550a64d2cc261981
Gotoshunya <1800syunya@Gmail.com>
2021-07-19 00:16:27 +0900

    7/19 四則演算プログラム　改良の余地あり

--- JavaWcp/src/classMethod/calculator/SPI.java
+++ JavaWcp/src/classMethod/calculator/SPI.java
@@ -2,6 +2,7 @@ package classMethod.calculator;
 
 import java.util.Scanner;
 
+import classMethod.calculator.model.Calculate;
 import classMethod.calculator.model.Divisor;
 import classMethod.calculator.model.PerfectNumber;
 
@@ -10,18 +11,11 @@ public class SPI {
 		Scanner scanner = new Scanner(System.in);
 		Divisor divisor = new Divisor();
 		PerfectNumber perfectNumber = new PerfectNumber();
+		Calculate calculate = new Calculate();
 
-		System.out.println("選んでください　約数、　完全数");
+		System.out.println("選んでください　約数、　完全数、　四則演算");
 		String calculator = scanner.next();
-		boolean oneMore;
-
-		if (calculator.equals("約数")) {
-			oneMore = false;
-		} else if (calculator.equals("完全数")) {
-			oneMore = false;
-		} else {
-			oneMore = true;
-		}
+		boolean oneMore = true;
 
 		do {
 
@@ -34,12 +28,19 @@ public class SPI {
 			case "完全数":
 				perfectNumber.perfectNumber();
 				break;
+				
+			case "四則演算":
+				calculate.calculate();
+				break;
 			}
-
+			
 			if (calculator.equals("約数")) {
 				scanner.close();
 				oneMore = false;
 			} else if (calculator.equals("完全数")) {
+				scanner.close();
+				oneMore = false;
+			} else if (calculator.equals("四則演算")) {
 				scanner.close();
 				oneMore = false;
 			} else {
---
+++ JavaWcp/src/classMethod/calculator/model/Calculate.java
@@ -0,0 +1,113 @@
+package classMethod.calculator.model;
+
+import java.util.Scanner;
+
+public class Calculate {
+	int num = 0;
+	int counter = 0;
+
+	public void calculate() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("1つ目の整数を記入: ");
+		int number = scanner.nextInt();
+		this.num = number;
+
+		Scanner scan = new Scanner(System.in);
+		String symbol = " ";
+
+		int count = 0;
+		boolean whatSymbol = true;
+
+		do {
+
+			if (count >= 1) {
+				System.out.print("前の整数を変化させる整数を記入: ");
+				number = scanner.nextInt();
+
+				System.out.print("前の整数を「+,-,*,/,」で処理する。 「+=, -=, *=, /=」で処理して計算を終了する。: ");
+				symbol = scanner.next();
+			}
+
+			switch (symbol) {
+			case "+":
+				addNumber(number);
+				count++;
+				break;
+
+			case "+=":
+				addNumber(number);
+				whatSymbol = false;
+				scanner.close();
+				scan.close();
+				break;
+
+			case "-":
+				minusNumber(number);
+				count++;
+				break;
+
+			case "-=":
+				minusNumber(number);
+				whatSymbol = false;
+				scanner.close();
+				scan.close();
+				break;
+
+			case "*":
+				timsNumber(number);
+				count++;
+				break;
+
+			case "*=":
+				timsNumber(number);
+				whatSymbol = false;
+				scanner.close();
+				scan.close();
+				break;
+
+			case "/":
+				divisionNumber(number);
+				count++;
+				break;
+
+			case "/=":
+				divisionNumber(number);
+				whatSymbol = false;
+				scanner.close();
+				scan.close();
+				break;
+
+			case " ":
+				count++;
+				this.counter++;
+				break;
+
+			default:
+				System.out.println("演算記号以外記入しないでください。整数・記号を打ち直してください。");
+			}
+		} while (whatSymbol);
+
+		System.out.println("計算結果は　" + this.num);
+
+	}
+
+	public int addNumber(int number) {
+			this.num += number;
+			return this.num;
+	}
+
+	public int minusNumber(int number) {
+			this.num -= number;
+			return this.num;
+	}
+
+	public int timsNumber(int number) {
+			this.num *= number;
+			return this.num;
+	}
+
+	public int divisionNumber(int number) {
+			this.num /= number;
+			return this.num;
+	}
+}
